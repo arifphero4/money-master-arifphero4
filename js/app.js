ddf21e1b9b1414cb8363178b6b68ddea7a83f62f
arifphero <ahammadarif232@gmail.com>
2022-02-17 20:27:48 +0600

    code error

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
-const incomeInput = document.getElementById("income-input");
-const foodInput = document.getElementById("food-input");
-const rentInput = document.getElementById("rent-input");
-const othersInput = document.getElementById("others-input");
+const incomeInput = document.getElementById("income-input").value;
+const foodInput = document.getElementById("food-input").value;
+const rentInput = document.getElementById("rent-input").value;
+const othersInput = document.getElementById("others-input").value;
 const calcButton = document.getElementById("calculate-btn");
-const totalExpenses = document.getElementById("total-expenses");
-const balanceField = document.getElementById("balance-field");
-const saveInput = document.getElementById("save-input");
+const totalExpenses = document.getElementById("total-expenses").value;
+const balanceField = document.getElementById("balance-field").value;
+const saveInput = document.getElementById("save-input").value;
 const saveButton = document.getElementById("save-btn");
-const saveAmount = document.getElementById("save-amount");
-const finalBalance = document.getElementById("rest-balance");
+const saveAmount = document.getElementById("save-amount").value;
+const finalBalance = document.getElementById("rest-balance").value;
+
+// error messages input
 const numberError = document.getElementById("notify-fail");
 
 let totalIncome;
@@ -16,35 +18,42 @@ let updateBalance;
 let totalCost;
 
 calcButton.addEventListener("click", function () {
-  totalIncome = parseFloat(incomeInput.value);
-  const foodCost = parseFloat(foodInput.value);
-  const rentCost = parseFloat(rentInput.value);
-  const othersCost = parseFloat(othersInput.value);
-
-  if (totalIncome < 0 || foodCost < 0 || rentCost < 0 || othersCost < 0) {
-    const errorMessage = numberError.innerText;
-    errorMessage.style.display = "block";
+  if (
+    typeof incomeInput !== "number" &&
+    typeof foodInput !== "number" &&
+    typeof rentInput !== "number" &&
+    typeof othersInput !== "number"
+  ) {
+    numberError.style.display = "block ";
+    console.log(numberError);
   } else {
-    // update total expenses
-    totalCost = foodCost + rentCost + othersCost;
-    totalExpenses.innerText = totalCost;
-    //   update balance
-    updateBalance = totalIncome - totalCost;
-    balanceField.innerText = updateBalance;
+    console.log("number pawa gese");
   }
+
+  /*  totalIncome = Number(incomeInput);
+  const foodCost = Number(foodInput);
+  const rentCost = Number(rentInput);
+  const othersCost = Number(othersInput);
+
+  // update total expenses
+  totalCost = foodCost + rentCost + othersCost;
+  totalExpenses.innerText = totalCost;
+  //   update balance
+  updateBalance = totalIncome - totalCost;
+  balanceField.innerText = updateBalance;
 
   //   clear input field
   incomeInput.value = "";
   foodInput.value = "";
   rentInput.value = "";
-  othersInput.value = "";
+  othersInput.value = ""; */
 });
 
 saveButton.addEventListener("click", function () {
-  const saveParcent = parseFloat(saveInput.value);
+  /* const saveParcent = Number(saveInput);
   const savingMoney = (totalIncome * saveParcent) / 100;
   saveAmount.innerText = savingMoney;
-  console.log(totalIncome);
+  // console.log(totalIncome);
 
   //update remaining balance
   const remainingBalance = updateBalance - savingMoney;
@@ -52,5 +61,5 @@ saveButton.addEventListener("click", function () {
   finalBalance.innerText = remainingBalance;
   // clear input field
   incomeInput.value = "";
-  saveInput.value = "";
+  saveInput.value = ""; */
 });
