942d18fe7951ff3b150ca34ba07d7080d5a28164
arifphero <ahammadarif232@gmail.com>
2022-02-17 15:04:01 +0600

    error handling

--- js/app.js
+++ js/app.js
@@ -9,22 +9,29 @@ const saveInput = document.getElementById("save-input");
 const saveButton = document.getElementById("save-btn");
 const saveAmount = document.getElementById("save-amount");
 const finalBalance = document.getElementById("rest-balance");
+const numberError = document.getElementById("notify-fail");
 
 let totalIncome;
 let updateBalance;
+let totalCost;
 
-calcButton.addEventListener("click", function getData() {
+calcButton.addEventListener("click", function () {
   totalIncome = parseFloat(incomeInput.value);
   const foodCost = parseFloat(foodInput.value);
   const rentCost = parseFloat(rentInput.value);
   const othersCost = parseFloat(othersInput.value);
 
-  // update total expenses
-  const totalCost = foodCost + rentCost + othersCost;
-  totalExpenses.innerText = totalCost;
-  //   update balance
-  updateBalance = totalIncome - totalCost;
-  balanceField.innerText = updateBalance;
+  if (totalIncome < 0 || foodCost < 0 || rentCost < 0 || othersCost < 0) {
+    const errorMessage = numberError.innerText;
+    errorMessage.style.display = "block";
+  } else {
+    // update total expenses
+    totalCost = foodCost + rentCost + othersCost;
+    totalExpenses.innerText = totalCost;
+    //   update balance
+    updateBalance = totalIncome - totalCost;
+    balanceField.innerText = updateBalance;
+  }
 
   //   clear input field
   incomeInput.value = "";
